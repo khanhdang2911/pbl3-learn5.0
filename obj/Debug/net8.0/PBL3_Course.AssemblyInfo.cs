ce5fe8ae1fde2306325026074e6bb9ce405a096d
Dang Xuan Khanh <khanhdang3152@gmail.com>
2024-04-14 20:27:00 +0700

    build post comment/star-rating

--- obj/Debug/net8.0/PBL3_Course.AssemblyInfo.cs
+++ obj/Debug/net8.0/PBL3_Course.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBL3_Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c70a326d391fd4c0595e62f1bee106d9e284645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc24b66f9bc5b9544cfb19040091791e8f096c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBL3_Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBL3_Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
